613ed3e724b2cba9013f5a9d29ea59b8cb357fe7
zarexu <klid16@gmail.com>
2015-06-03 17:55:01 +0200

    Uzupelniony calc.cpp

--- src/calc.cpp
+++ src/calc.cpp
@@ -2,14 +2,18 @@
 
 int Calculator::pow(int a, int n)
 {
+	int liczba=a;
 	// ZAIMPLEMENTUJ MNIE ;_;
-
+	for(int i = 1; i <= n/2; i++)
+		liczba=liczba*liczba;
 	return -1;
 }
 	
 int Calculator::fac(int n)
 {
+	int liczba = 1;
 	// ZAIMPLEMENTUJ MNIE ;_;
-
+	for(int i = 1; i <= n; i++)
+		liczba = liczba * i;
 	return -1;
 }
